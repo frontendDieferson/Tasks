c675f2aaf272d641f4e9564b8fbe19b2445faaaa
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2021-10-18 10:11:26 -0300

    Features Finish

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+      <h1>Minhas Tarefas</h1>
+      <tasks-progress :progress="progress"></tasks-progress>
+		  <new-task @taskAdded="addTask"></new-task>
+		  <task-grid :tasks="tasks" @taskDeleted="deleteTask" @taskStateChanged="toggleTaskState"></task-grid>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskGrid from '@/components/TaskGrid.vue'
+import NewTask from '@/components/NewTask.vue'
+import TasksProgress from '@/components/TasksProgress'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+	components: {
+		TaskGrid, NewTask, TasksProgress
+	},
+	data() {
+		return {
+			tasks: []
+		}
+	},
+	methods: {
+		addTask(task) {
+			const sameName = t => t.name === task.name
+			const reallyNew = this.tasks.filter(sameName).length == 0
+			if(reallyNew) {
+				this.tasks.push({
+					name: task.name,
+					pending: task.pending || true
+				})
+			}
+		},
+		deleteTask(i) {
+			this.tasks.splice(i, 1)
+		},
+		toggleTaskState(i) {
+			this.tasks[i].pending = !this.tasks[i].pending
+		}
+	},
+	computed: {
+		progress() {
+			const total = this.tasks.length
+			const done = this.tasks.filter(t => !t.pending).length
+			return Math.round(done / total * 100) || 0
+		}
+	},
+	watch: {
+		tasks: {
+			deep: true,
+			handler() {
+				localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			}
+		}
+	},
+	created() {
+		const json = localStorage.getItem('tasks')
+		this.tasks = JSON.parse(json) || []
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	body {
+		font-family: 'Lato', sans-serif;
+		background: linear-gradient(to right, rgb(22, 34, 42), rgb(58, 96, 115));
+		color: #FFF;
+	}
+	#app {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+	#app h1 {
+		margin-bottom: 20px;
+		font-weight: bold;
+		font-size: 3rem;
+	}
 </style>
